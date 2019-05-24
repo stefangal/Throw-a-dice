d006e6f99ece3b56b20bf057863d689f397c4f96
SefPY <46818022+stefangal@users.noreply.github.com>
2019-05-24 07:39:38 +0200

    Added 2 players to play dice

--- Throw a dice - terminal version.py
+++ Throw a dice - terminal version.py
@@ -50,13 +50,26 @@ cubes = [
 
 running = True
 t = 0
+orig_cube = cubes[:]
+os.system('cls' if os.name == 'nt' else 'clear')
+p1 = int(input('Player 1 guess [1-6]: '))
+p2 = int(input('Player 2 guess [1-6]: '))
 while running:
-    t += 0.01
-    os.system('cls' if os.name == 'nt' else 'clear')
-    random.shuffle(cubes)
-    print(cubes[0])
-    time.sleep(t)
-    if t >= 0.3:
-        print()
-        print("Ready")
-        running = False
+   t += 0.01
+   os.system('cls' if os.name == 'nt' else 'clear')
+   print(f'Player 1 guess is {p1}')
+   print(f'Player 2 guess is {p2}')
+   random.shuffle(cubes)
+   print(cubes[0])
+   res = orig_cube.index(cubes[0])+1
+   time.sleep(t)
+   if t >= 0.3:
+      print()
+      print("Your dice is nr: ",res)
+      running = False
+if abs(p1-res) < abs(p2 - res):
+   print("THE WINNER IS PLAYER 1 !!!")
+elif abs(p1-res) > abs(p2 - res):
+   print("THE WINNER IS PLAYER 2 !!!")
+else:
+   print("IT IS A DRAW !!!")
